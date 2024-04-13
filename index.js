403926fdc3563c861a271dd62b50953f7c485be0
Sakti Book <166900619+saktibook@users.noreply.github.com>
2024-04-13 12:24:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+tsend.onclick=()=>{
+    if(xname.value=="" && email.value=="" && tarea.value=="" && sub.value=="")
+    {
+        alert("Please Enter a Information");
+    }
+    else{
+        alert("Thank you")
+        
+    }
+}
